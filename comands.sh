c66098f3c0362af273b0466891af18fe345e7efa
LizethE <LizethE@sicingenieria.net>
2024-10-05 13:38:35 -0500

    actualizo file comands user_data

--- comands.sh
+++ comands.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
+# Actualizamos el sistema actual e instalamos los requisitos
 sudo yum update -y
 
-# Iniciar Docker
-sudo systemctl start docker
+# Instalamos Docker en la instancia
+sudo yum install docker -y
 
-# Habilitar Docker para que se inicie al arrancar el sistema
+# Iniciamos Docker dentro de la instancia
+sudo systemctl start docker 
+
+# Agregamos el usuario ec2-user al grupo docker para ejecutar sin sudo
+sudo usermod -a -G docker ec2-user
+
+# Habilitamos Docker para iniciar con el sistema
 sudo systemctl enable docker
 
-# Añadir el usuario 'ec2-user' al grupo docker para poder ejecutar Docker sin sudo
-sudo usermod -aG docker ec2-user
-
-
-# Instalar Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Dar permisos de ejecución a Docker Compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Descargar la imagen de Docker que necesitas (por ejemplo, la imagen de Nginx)
-sudo docker pull nginx:latest
-
-# Opcional: Ejecutar un contenedor con la imagen descargada (en este caso, Nginx)
-sudo docker run -d -p 80:80 --name my_nginx_container nginx:latest
-
-# Verificar que Docker y Docker Compose se instalaron correctamente
-docker --version
-docker-compose --version
-
-# Verificar que la imagen se ha descargado correctamente
-sudo docker images
+# Descargamos e iniciamos el contenedor de Nginx
+sudo docker run -d -p 80:80 --name nginx-container nginx
 
  
