d4e26a6ffe1a7c5230ecf0b4e7fd9ca4254a0529
LizethE <LizethE@sicingenieria.net>
2024-10-11 00:06:33 -0500

    actualizo file main para agregar balancesdores de carga  y bdRDS

--- main.tf
+++ main.tf
@@ -170,4 +170,92 @@ output "public_instance_1_ip" {
 output "public_instance_2_ip" {
   value = aws_instance.public_instance_2.public_ip
 }
-  
+
+  resource "aws_lb" "app_lb" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.public_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+
+  enable_deletion_protection = false
+  tags = {
+    Name = "AppLoad_Balancer"
+  }
+}
+
+
+# Creo un grupo de destinos para mis instancias
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.cloud2_vpc.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+  }
+  tags = {
+    Name = "AppTarget_Group"
+  }
+}
+
+# Registrar instancias EC2 en el grupo de destinos
+resource "aws_lb_target_group_attachment" "public_instance_1_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.public_instance_1.id
+  port             = 80
+}
+
+
+resource "aws_lb_target_group_attachment" "public_instance_2_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.public_instance_2.id
+  port             = 80
+}
+
+# Crear un listener para el balanceador de carga
+resource "aws_lb_listener" "app_lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Crear el grupo de subredes para la base de datos RDS
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "my_db_subnet_group"
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id] # Usar las subredes privadas
+
+  tags = {
+    Name = "my_db_subnet_group"
+  }
+}
+
+# Crear la base de datos RDS 
+resource "aws_db_instance" "my_database" {
+   allocated_storage    = 20  # Tamaño del almacenamiento en GB, dentro del límite gratuito
+  storage_type       = "gp2"
+  engine             = "mysql" 
+  engine_version     = "8.0.39"  
+  instance_class     = "db.t3.micro" # Asegúrate de usar una instancia elegible para la capa gratuita
+  db_name            = "dbCloud2" 
+  username           = "admin"
+  password           = "Admin2024" 
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = [aws_security_group.public_sg.id] 
+  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
+                                               
+  tags = {
+    Name = "MyRDSInstance"
+  }
+}
